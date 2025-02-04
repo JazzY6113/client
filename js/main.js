700ddccd3714d49edefe046ee80d487d8687f629
JazzY6113 <kbogomolov22@gmail.com>
2025-02-04 11:44:36 +0700

    Практическая работа №11

--- js/main.js
+++ js/main.js
@@ -4,16 +4,24 @@
         this.reviews.push(productReview)
      }.bind(this))
     
-    Vue.component('product-tabs', {
+     Vue.component('product-tabs', {
         props: {
             reviews: {
                 type: Array,
                 required: false
+            },
+            shippingCost: {
+                type: String,
+                required: true
+            },
+            details: {
+                type: Array,
+                required: true
             }
         },
         data() {
             return {
-                tabs: ['Reviews', 'Make a Review'],
+                tabs: ['Reviews', 'Make a Review', 'Shipping', 'Details'],
                 selectedTab: 'Reviews'
             }
         },
@@ -38,6 +46,14 @@
                 </div>
                 <div v-show="selectedTab === 'Make a Review'">
                     <product-review></product-review>
+                </div>
+                <div v-show="selectedTab === 'Shipping'">
+                    <p>Стоимость доставки: {{ shippingCost }}</p>
+                </div>
+                <div v-show="selectedTab === 'Details'">
+                    <ul>
+                        <li v-for="detail in details">{{ detail }}</li>
+                    </ul>
                 </div>
             </div>
         `,
@@ -123,121 +139,127 @@
     })
 
     Vue.component('product', {
-    props: {
-        premium: {
-            type: Boolean,
-            required: true
-        }
-    },
-    template: `
-    <div class="product">
-        <div class="product-image">
-            <img :src="image" :alt="altText"/>
+        props: {
+            premium: {
+                type: Boolean,
+                required: true
+            }
+        },
+        template: `
+        <div class="product">
+            <div class="product-image">
+                <img :src="image" :alt="altText"/>
+            </div>
+    
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p>{{ description }}</p>
+                <p v-if="inStock" >В наличии</p>
+                <p v-else :class="{ OutOfStock:!inStock }">Нет в наличии</p>
+                <span>{{ sale }}</span>
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+                <select>
+                    <option v-for="size in sizes">{{size}}</option>
+                </select>
+                <a v-bind:href="link">Похожее</a>
+                <p>Доставка: {{ shipping }}</p>
+                <div
+                        class="color-box"
+                        v-for="(variant, index) in variants"
+                        :key="variant.variantId"
+                        :style="{ backgroundColor:variant.variantColor }"
+                        @mouseover="updateProduct(index)"
+                ></div>
+                
+                <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Добавить в корзину</button>
+                <button v-on:click="removeToCart">Удалить из корзины</button>
+            </div>           
+            <div>
+                <product-tabs 
+                    :reviews="reviews" 
+                    :shipping-cost="shipping" 
+                    :details="details"
+                    @review-submitted="addReview">
+                </product-tabs>
+            </div>
         </div>
-
-        <div class="product-info">
-            <h1>{{ title }}</h1>
-            <p>{{ description }}</p>
-            <p v-if="inStock" >В наличии</p>
-            <p v-else :class="{ OutOfStock:!inStock }">Нет в наличии</p>
-            <span>{{ sale }}</span>
-            <ul>
-                <li v-for="detail in details">{{ detail }}</li>
-            </ul>
-            <select>
-                <option v-for="size in sizes">{{size}}</option>
-            </select>
-            <a v-bind:href="link">Похожее</a>
-            <p>Доставка: {{ shipping }}</p>
-            <div
-                    class="color-box"
-                    v-for="(variant, index) in variants"
-                    :key="variant.variantId"
-                    :style="{ backgroundColor:variant.variantColor }"
-                    @mouseover="updateProduct(index)"
-            ></div>
-            
-            <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Добавить в корзину</button>
-            <button v-on:click="removeToCart">Удалить из корзины</button>
-        </div>           
-        <div>
-            <product-tabs :reviews="reviews" @review-submitted="addReview"></product-tabs>
-        </div>
-    `,
-    data() {
-        return {
-            product: "Носки",
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            description: "Пара теплых, пушистых носков.",
-            link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-            onSale: true,
-            altText: "Пара носков",
-            details: ['80% хлопок', '20% полиэстер', 'унисекс'],
-            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-            variants: [
-                {
-                    variantId: 2234,
-                    variantColor: 'green',
-                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                    variantQuantity: 10
-                },
-                {
-                    variantId: 2235,
-                    variantColor: 'blue',
-                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                    variantQuantity: 0
+        `,
+        data() {
+            return {
+                product: "Носки",
+                brand: 'Vue Mastery',
+                selectedVariant: 0,
+                description: "Пара теплых, пушистых носков.",
+                link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+                onSale: true,
+                altText: "Пара носков",
+                details: ['80% хлопок', '20% полиэстер', 'унисекс'],
+                sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+                variants: [
+                    {
+                        variantId: 2234,
+                        variantColor: 'green',
+                        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                        variantQuantity: 10
+                    },
+                    {
+                        variantId: 2235,
+                        variantColor: 'blue',
+                        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                        variantQuantity: 0
+                    }
+                ],
+                reviews: []
+            }
+        },
+        methods: {
+            addToCart() {
+                this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+            },
+            removeToCart(index) {
+                this.$emit('remove-to-cart', this.variants[this.selectedVariant].variantId);
+            },
+            updateProduct(index) {
+                this.selectedVariant = index;
+                console.log(index);
+            },
+            addReview(productReview) {
+                this.reviews.push(productReview)
+            }
+        },
+        computed: {
+            title() {
+                return this.brand + ' ' + this.product;
+            },
+            image() {
+                return this.variants[this.selectedVariant].variantImage;
+            },
+            inStock() {
+                return this.variants[this.selectedVariant].variantQuantity
+            },
+            sale() {
+                if (this.onSale) {
+                    return this.brand + ' ' + this.product + ' ' + 'действует распродажа';
+                } else {
+                    return this.brand + ' ' + this.product + ' ' + 'не действует распродажи';
                 }
-            ],
-            reviews: []
-        }
-    },
-    methods: {
-        addToCart() {
-            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
-        },
-        removeToCart(index) {
-            this.$emit('remove-to-cart', this.variants[this.selectedVariant].variantId);
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-            console.log(index);
-        },
-        addReview(productReview) {
-            this.reviews.push(productReview)
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].variantQuantity
-        },
-        sale() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' ' + 'действует распродажа';
-            } else {
-                return this.brand + ' ' + this.product + ' ' + 'не действует распродажи';
+            },
+            shipping() {
+                if (this.premium) {
+                    return "Бесплатно";
+                } else {
+                    return "2.99$";
+                }
             }
         },
-        shipping() {
-            if (this.premium) {
-                return "Бесплатно";
-            } else {
-                return 2.99
-            }
-        }
-    },
-    mounted() {
-        eventBus.$on('review-submitted', productReview => {
-            this.reviews.push(productReview)
-        })
-    }     
-    })
+        mounted() {
+            eventBus.$on('review-submitted', productReview => {
+                this.reviews.push(productReview)
+            })
+        }     
+    });
 
     let app = new Vue({
     el: '#app',
